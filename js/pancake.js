98d20368ba449b90554edbcef4345ae0e32856dd
Ruben <rubygaming6@gmail.com>
2021-02-23 11:28:01 -0500

    Update pancake.js
    skeleton for js file

--- js/pancake.js
+++ js/pancake.js
@@ -1,1 +1,46 @@
 //This is the beginning of our terrible, awful, no good, very bad game.
+
+class gameScene extends Phaser.Scene{
+    constructor() {
+        super({
+            key: 'gameScene',
+            active: true
+        });
+ 
+        this.cursor = new Phaser.Math.Vector2();
+ 
+        this.playerSpeed = 0.1;
+        this.enemyMaxY = 280;
+        this.enemyMinY = 80;
+    }
+
+
+preload(){
+
+}
+
+create(){
+
+}
+
+update(){
+ }
+}
+
+// our game's configuration
+let config = {
+    type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
+    width: 640, // game width
+    height: 360, // game height
+    scene: gameScene, // our newly created scene
+    parent: 'main-game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false
+        }
+    }
+};
+ 
+// create the game, and pass it the configuration
+let game = new Phaser.Game(config);
