e630ed0e255133aae0a9618c6a0bbfcc723bb2a3
Michael Gublo <mgublo@holyghostprep.org>
2021-03-03 08:38:05 -0500

    switching scene and menu work

--- js/pancake.js
+++ js/pancake.js
@@ -1,5 +1,43 @@
 //This is the beginning of our terrible, awful, no good, very bad game.
-
+class SimpleScene extends Phaser.Scene {
+    constructor() {
+        super({key: 'SimpleScene'});
+    }
+    create() {
+      let clickCount = 0;
+      this.clickCountText = this.add.text(100, 200, '');
+  
+      this.clickButton = this.add.text(100, 100, 'Click me!', { fill: '#0f0' })
+        .setInteractive()
+        .on('pointerover', () => this.enterButtonHoverState() )
+        .on('pointerout', () => this.enterButtonRestState() )
+        .on('pointerdown', () => this.enterButtonActiveState() )
+        .on('pointerup', () => {
+          this.updateClickCountText(++clickCount);
+          this.enterButtonHoverState();
+          this.scene.start('titleScene');
+          console.log('scencechange')
+      });
+  
+      this.updateClickCountText(clickCount);
+    }
+  
+    updateClickCountText(clickCount) {
+      this.clickCountText.setText(`Button has been clicked ${clickCount} times.`);
+    }
+  
+    enterButtonHoverState() {
+      this.clickButton.setStyle({ fill: '#ff0'});
+    }
+  
+    enterButtonRestState() {
+      this.clickButton.setStyle({ fill: '#0f0' });
+    }
+  
+    enterButtonActiveState() {
+      this.clickButton.setStyle({ fill: '#0ff' });
+    }
+    }
 class titleScene extends Phaser.Scene{
     constructor() {
         super({key: 'titleScene'});
@@ -7,47 +45,17 @@ class titleScene extends Phaser.Scene{
     preload () {
         this.load.image('background_button', 'img/StartButton.jpg')
     }
-    //askdoaskdosa
     create () {
         let background = this.add.sprite(0, 0, 'background_button');
         background.setOrigin(0,0);
-    
     }
 }
 
-class gameScene extends Phaser.Scene{
-    constructor() {
-        super({
-            key: 'gameScene',
-            active: true
-        });
- 
-        this.cursor = new Phaser.Math.Vector2();
- 
-        this.playerSpeed = 0.1;
-        this.enemyMaxY = 280;
-        this.enemyMinY = 80;
-    }
-
-
-preload(){
-    
-}
-
-create(){
-
-}
-
-update(){
- }
-}
-
-// our game's configuration
-let config = {
+    let config = {
     type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
     width: 640, // game width
     height: 360, // game height
-    scene: gameScene, // our newly created scene
+    scene: [SimpleScene, titleScene], // our newly created scene
     parent: 'main-game',
     physics: {
         default: 'arcade',
@@ -56,8 +64,4 @@ let config = {
         }
     }
 };
- 
-// create the game, and pass it the configuration
-let TitleScene = new TitleScene('Title');
-
 let game = new Phaser.Game(config);
