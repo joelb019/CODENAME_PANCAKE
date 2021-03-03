f894f4d1eb874d381bfbcf2ef8c9a7966c06f7ad
Luke Borkowski <Lukeski902@gmail.com>
2021-03-03 13:54:54 -0500

    Update pancake.js
    - Cleaned up file so it looks nicer
    - Added mainScene class
    - Made start button switch to the main scene
    - loaded background and earth to mainScene

--- js/pancake.js
+++ js/pancake.js
@@ -1,8 +1,11 @@
 //This is the beginning of our terrible, awful, no good, very bad game.
+
 class SimpleScene extends Phaser.Scene {
+
     constructor() {
         super({key: 'SimpleScene'});
     }
+
     create() {
       let clickCount = 0;
       this.clickCountText = this.add.text(100, 200, '');
@@ -16,7 +19,7 @@ class SimpleScene extends Phaser.Scene {
           this.updateClickCountText(++clickCount);
           this.enterButtonHoverState();
           this.scene.start('titleScene');
-          console.log('scencechange')
+          console.log('Change Scene to Title');
       });
   
       this.updateClickCountText(clickCount);
@@ -37,21 +40,52 @@ class SimpleScene extends Phaser.Scene {
     enterButtonActiveState() {
       this.clickButton.setStyle({ fill: '#0ff' });
     }
-    }
-class titleScene extends Phaser.Scene{
+}
+
+class titleScene extends Phaser.Scene {
+
     constructor() {
         super({key: 'titleScene'});
     }
-    preload () {
-        this.load.image('background_button', 'img/StartButton.jpg')
+
+    preload() {
+        this.load.image('background_button', 'img/StartButton.jpg');
     }
-    create () {
-        let background = this.add.sprite(0, 0, 'background_button');
-        background.setOrigin(0,0);
+
+    create() {
+        this.clickButton = this.add.sprite(0, 0, 'background_button')
+        .setOrigin(0, 0)
+        .setInteractive()
+        .on('pointerup', () => {
+          this.scene.start('mainScene');
+          console.log('Chance Scene to Main');
+        });
     }
 }
 
-    let config = {
+class mainScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: 'mainScene'});
+    }
+
+    preload() {
+        this.load.image('background', 'img/bgPlaceholder.jpg');
+        this.load.image('planet', 'img/earthPlaceholder.png');
+    }
+
+    create() {
+        let background = this.add.sprite(0, 0, 'background');
+        background.setOrigin(0, 0);
+
+        let planet = this.add.sprite(0, 0, 'planet');
+        planet.setOrigin(0, 330);
+
+        this.add.text(100, 100, 'testing text');
+    }
+}
+
+let config = {
     type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
     width: 640, // game width
     height: 360, // game height
@@ -64,4 +98,5 @@ class titleScene extends Phaser.Scene{
         }
     }
 };
+
 let game = new Phaser.Game(config);
