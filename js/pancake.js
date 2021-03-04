b95f81c11328617f6592ef1388f92c3b61bf8f6a
Michael Gublo <mgublo@holyghostprep.org>
2021-03-04 09:33:23 -0500

    gamestart menu+fixed html and style

--- js/pancake.js
+++ js/pancake.js
@@ -1,36 +1,37 @@
 //This is the beginning of our terrible, awful, no good, very bad game.
 
-class SimpleScene extends Phaser.Scene {
-
+class mainMenu extends Phaser.Scene {
     constructor() {
-        super({key: 'SimpleScene'});
+        super({key: 'mainMenu'});
+    }
+
+    preload() {
+      this.load.image('background', 'img/bgPlaceholder.jpg');
+      this.load.image('planet', 'img/earthPlaceholder.png');
     }
 
     create() {
-      let clickCount = 0;
-      this.clickCountText = this.add.text(100, 200, '');
-  
-      this.clickButton = this.add.text(100, 100, 'Click me!', { fill: '#0f0' })
+      let background = this.add.sprite(35, 0, 'background');
+      background.setOrigin(0, 0);
+
+      this.add.text(45, 50, 'CODENAME: PANCAKE', { font: '50px Arial', align: 'center' });
+      this.add.text(55, 300, 'Use the left and right arrow keys to move the paddle and protect the planet!', { font: '16px Arial', align: 'center' })
+
+      this.clickButton = this.add.text(260, 200, 'START GAME!', { fill: '#0f0' })
         .setInteractive()
         .on('pointerover', () => this.enterButtonHoverState() )
         .on('pointerout', () => this.enterButtonRestState() )
         .on('pointerdown', () => this.enterButtonActiveState() )
         .on('pointerup', () => {
-          this.updateClickCountText(++clickCount);
           this.enterButtonHoverState();
-          this.scene.start('titleScene');
-          console.log('Change Scene to Title');
+          this.scene.start('gameScene');
+          console.log('Change Scene to game');
       });
-  
-      this.updateClickCountText(clickCount);
+
     }
-  
-    updateClickCountText(clickCount) {
-      this.clickCountText.setText(`Button has been clicked ${clickCount} times.`);
-    }
-  
+
     enterButtonHoverState() {
-      this.clickButton.setStyle({ fill: '#ff0'});
+      this.clickButton.setStyle({ fill: '#ff0' });
     }
   
     enterButtonRestState() {
@@ -42,31 +43,9 @@ class SimpleScene extends Phaser.Scene {
     }
 }
 
-class titleScene extends Phaser.Scene {
-
+class gameScene extends Phaser.Scene {
     constructor() {
-        super({key: 'titleScene'});
-    }
-
-    preload() {
-        this.load.image('background_button', 'img/StartButton.jpg');
-    }
-
-    create() {
-        this.clickButton = this.add.sprite(0, 0, 'background_button')
-        .setOrigin(0, 0)
-        .setInteractive()
-        .on('pointerup', () => {
-          this.scene.start('mainScene');
-          console.log('Chance Scene to Main');
-        });
-    }
-}
-
-class mainScene extends Phaser.Scene {
-
-    constructor() {
-        super({key: 'mainScene'});
+        super({key: 'gameScene'});
     }
 
     preload() {
@@ -81,7 +60,7 @@ class mainScene extends Phaser.Scene {
         let planet = this.add.sprite(0, 0, 'planet');
         planet.setOrigin(0, 330);
 
-        this.add.text(100, 100, 'testing text');
+        this.add.text(100, 100, 'game');
     }
 }
 
@@ -89,7 +68,7 @@ let config = {
     type: Phaser.AUTO, //Phaser will decide how to render our game (WebGL or Canvas)
     width: 640, // game width
     height: 360, // game height
-    scene: [SimpleScene, titleScene], // our newly created scene
+    scene: [mainMenu, gameScene], 
     parent: 'main-game',
     physics: {
         default: 'arcade',
